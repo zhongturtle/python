9c5c6f5616205a1f4f78ce9953898061d2304a19
zhongturtle <zhongturtle@gmail.com>
2017-01-19 17:04:14 +0800

    add some simple funtion about waring

--- send_email/send_email.py
+++ send_email/send_email.py
@@ -15,3 +15,31 @@ email_conn.login(username,password) #login
 email_conn.sendmail(from_email, to_list, "hello there this is an email message")#send email ,which's format like from "from_email" to "to_list" with text "hello there there"
 
 email_conn.quit() #disconnect
+
+from smtplib import SMTP
+
+
+ABC = SMTP(host, port)
+ABC.ehlo()
+ABC.starttls()
+ABC.login(username, password)
+ABC.sendmail(from_email, to_list, "Hello there this is an email message")
+ABC.quit()
+
+
+from smtplib import SMTP, SMTPAuthenticationError, SMTPException
+
+
+pass_wrong = SMTP(host, port)
+pass_wrong.ehlo()
+pass_wrong.starttls()
+try:
+    pass_wrong.login(username, "wrong_password")
+    pass_wrong.sendmail(from_email, to_list, "Hello there this is an email message")
+except SMTPAuthenticationError:
+    print("Could not login")
+except:
+    print("an error occured")
+
+pass_wrong.quit()
+
